134e57596fa7c72350356289aa8446a2c74717a4
Tanmay <tanmaysri04@gmail.com>
2026-05-14 16:10:35 +0530

    Fix DB path for production

--- server/db.js
+++ server/db.js
@@ -1,7 +1,11 @@
 const Database = require('better-sqlite3');
 const path = require('path');
 
-const db = new Database(path.join(__dirname, 'kanban.db'));
+const DB_PATH = process.env.NODE_ENV === 'production'
+  ? '/app/data/kanban.db'
+  : path.join(__dirname, 'kanban.db');
+
+const db = new Database(DB_PATH);
 
 db.pragma('journal_mode = WAL');
 db.pragma('foreign_keys = ON');
