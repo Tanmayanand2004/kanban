1abb4b66b65de626673fb38162dd776830023313
Tanmay <tanmaysri04@gmail.com>
2026-05-14 15:49:05 +0530

    Fix PORT and task routes

--- server/index.js
+++ server/index.js
@@ -4,9 +4,10 @@ const path = require('path');
 
 const userRoutes  = require('./routes/users');
 const boardRoutes = require('./routes/boards');
+const taskRoutes  = require('./routes/tasks');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -15,14 +16,12 @@ app.use(express.static(path.join(__dirname, '../client')));
 app.use('/api/auth', userRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/boards', boardRoutes);
+app.use('/api/tasks', taskRoutes);
 
 app.get('/{*path}', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log('Server running at http://localhost:' + PORT);
 });
-const taskRoutes  = require('./routes/tasks');   // ADD
-// ...
-app.use('/api/tasks', taskRoutes);               // ADD
